11a387af8fb2d168622d32d433be179ba65e245e
iOSMitchSalcido <iosmitchsalcido@gmail.com>
2017-04-16 12:52:33 -0700

    Initial Commit

---
+++ TheMovieManager/TMDBMovie.swift
@@ -0,0 +1,63 @@
+//
+//  TMDBMovie.swift
+//  TheMovieManager
+//
+//  Created by Jarrod Parkes on 2/11/15.
+//  Copyright (c) 2015 Jarrod Parkes. All rights reserved.
+//
+
+// MARK: - TMDBMovie
+
+struct TMDBMovie {
+
+    // MARK: Properties
+    
+    let title: String
+    let id: Int
+    let posterPath: String?
+    let releaseYear: String?
+    let voteAverage: Float?
+    
+    // MARK: Initializers
+    
+    // construct a TMDBMovie from a dictionary
+    init(dictionary: [String:AnyObject]) {
+        
+        title = dictionary[TMDBClient.JSONResponseKeys.MovieTitle] as! String
+        id = dictionary[TMDBClient.JSONResponseKeys.MovieID] as! Int
+        posterPath = dictionary[TMDBClient.JSONResponseKeys.MoviePosterPath] as? String
+        
+        if let releaseDateString = dictionary[TMDBClient.JSONResponseKeys.MovieReleaseDate] as? String, releaseDateString.isEmpty == false {
+            releaseYear = releaseDateString.substring(to: releaseDateString.characters.index(releaseDateString.startIndex, offsetBy: 4))
+        } else {
+            releaseYear = ""
+        }
+        
+        if let vote = dictionary["vote_average"] as? Float {
+            voteAverage = vote
+        }
+        else {
+            voteAverage = nil
+        }
+    }
+    
+    static func moviesFromResults(_ results: [[String:AnyObject]]) -> [TMDBMovie] {
+        
+        var movies = [TMDBMovie]()
+        
+        // iterate through array of dictionaries, each Movie is a dictionary
+        for result in results {
+            movies.append(TMDBMovie(dictionary: result))
+        }
+        
+        return movies
+    }
+}
+
+// MARK: - TMDBMovie: Equatable
+
+extension TMDBMovie: Equatable {}
+
+func ==(lhs: TMDBMovie, rhs: TMDBMovie) -> Bool {
+    return lhs.id == rhs.id
+}
